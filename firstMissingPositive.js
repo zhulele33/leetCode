0bda1ce6ca7593f51bf6eb316f7d5aaf5e537afe
zhuchunle001 <zhuchunle001@lianjia.com>
2019-04-30 10:29:18 +0800

    缺失的第一个正数

---
+++ firstMissingPositive.js
@@ -0,0 +1,43 @@
+/**
+ * 缺失的第一个正数
+ * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+
+示例 1:
+
+输入: [1,2,0]
+输出: 3
+示例 2:
+
+输入: [3,4,-1,1]
+输出: 2
+示例 3:
+
+输入: [7,8,9,11,12]
+输出: 1
+说明:
+
+你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
+ * 
+ * 
+ */
+
+ /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    var newNums = []
+    var index = 0 
+    if(nums.length == 0) return 1
+    while(index != nums.length){
+        if(nums[index]>0){
+            newNums[nums[index]] = 1
+        }
+        index++
+    }
+    for(var i= 0 ;i<newNums.length;i++){
+        if(newNums[i]!==1 && i!==0) return i
+    }
+    if(newNums.length == 0) return 1
+    return newNums.length
+};
